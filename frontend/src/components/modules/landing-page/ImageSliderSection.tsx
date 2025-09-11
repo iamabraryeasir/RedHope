e7d631bc88928a6c9bb09e3b63436922102abc66
Abrar Yeasir <162937581+iamabraryeasir@users.noreply.github.com>
2025-09-12 00:22:28 +0600

    feat: Added the marque in the image slider section

--- frontend/src/components/modules/landing-page/ImageSliderSection.tsx
+++ frontend/src/components/modules/landing-page/ImageSliderSection.tsx
@@ -1,81 +1,29 @@
+import Marque from "react-fast-marquee";
+
 export default function ImageSliderSection() {
   return (
     <section className="w-full mt-25">
-      <div className="top-slider grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/01.jpg"
-            alt="Image 1"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
+      <Marque autoFill={true}>
+        <div className="up flex flex-row gap-4 items-center justify-center">
+          <img src="src/assets/slider-images/01.png" alt="" className="ml-4" />
+          <img src="src/assets/slider-images/02.png" alt="" />
+          <img src="src/assets/slider-images/03.png" alt="" />
+          <img src="src/assets/slider-images/04.png" alt="" />
+          <img src="src/assets/slider-images/05.png" alt="" />
+          <img src="src/assets/slider-images/03.png" alt="" />
         </div>
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/02.jpg"
-            alt="Image 2"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
-        </div>
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/03.jpg"
-            alt="Image 3"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
-        </div>
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/04.jpg"
-            alt="Image 4"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
-        </div>
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/05.jpg"
-            alt="Image 5"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
-        </div>
-      </div>
+      </Marque>
 
-      <div className="bottom-slider grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/05.jpg"
-            alt="Image 1"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
+      <Marque autoFill={true} direction="right">
+        <div className="down mt-5 flex flex-row gap-4 items-center justify-center">
+          <img src="src/assets/slider-images/05.png" alt="" className="ml-4" />
+          <img src="src/assets/slider-images/04.png" alt="" />
+          <img src="src/assets/slider-images/03.png" alt="" />
+          <img src="src/assets/slider-images/02.png" alt="" />
+          <img src="src/assets/slider-images/01.png" alt="" />
+          <img src="src/assets/slider-images/03.png" alt="" />
         </div>
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/04.jpg"
-            alt="Image 2"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
-        </div>
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/03.jpg"
-            alt="Image 3"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
-        </div>
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/02.jpg"
-            alt="Image 4"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
-        </div>
-        <div className="w-full h-60">
-          <img
-            src="src/assets/slider-images/01.jpg"
-            alt="Image 5"
-            className="w-full h-60 object-cover rounded-xl shadow-md"
-          />
-        </div>
-      </div>
+      </Marque>
     </section>
   );
 }
