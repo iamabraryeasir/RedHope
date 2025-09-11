11d9c65d84d77231978d516cc1dfa02e9e078263
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-09-11 11:25:21 +0600

    completed Blood Type Compatibility Guide

--- frontend/src/components/modules/landing-page/CompatibilityGuideSection.tsx
+++ frontend/src/components/modules/landing-page/CompatibilityGuideSection.tsx
@@ -1,3 +1,13 @@
 export default function CompatibilityGuide() {
-  return <section>CompatibilityGuide</section>;
+  return (
+    <section className="text-center mt-15 sm:mt-40">
+      <h2 className=" font-medium text-[25px] px-2 leading-[120%] tracking-[-1.5px] sm:text-[40px] lg:text-[50px] ">
+        Blood Type Compatibility Guide
+      </h2>
+      <p className="text-[#526061] text-[10px] px-2 mt-5 leading-[170%] sm:text-[15px] lg:text-[18px]  ">
+        Understanding blood type compatibility is crucial for safe donations.{" "}
+        <br /> Learn who you can donate to and receive from.
+      </p>
+    </section>
+  );
 }
