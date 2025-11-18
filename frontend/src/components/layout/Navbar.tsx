e2fcc7a89e0eee5a39c8eb33942c8212f682b40a
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-11-19 02:00:21 +0600

    mode removed

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -13,17 +13,16 @@ import Logo from "@/assets/Logo";
  */
 import { Button } from "@/components/ui/button";
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from "@/components/ui/popover";
-import { ModeToggle } from "./ModeToggle";
 import { useUserInfoQuery } from "@/redux/features/auth/auth.api";
 import NavUserIcon from "./NavUserIcon";
 
@@ -31,115 +30,126 @@ import NavUserIcon from "./NavUserIcon";
  * Navigation links array to be used in both desktop and mobile menus
  */
 const navigationLinks = [
-  { href: "/", label: "Home" },
-  { href: "/donors", label: "Donors" },
-  { href: "/requests", label: "Requests" },
-  { href: "/about", label: "About" },
-  { href: "/contact", label: "Contact Us" },
+    { href: "/", label: "Home" },
+    { href: "/donors", label: "Donors" },
+    { href: "/requests", label: "Requests" },
+    { href: "/about", label: "About" },
+    { href: "/contact", label: "Contact Us" },
 ];
 
 export default function Navbar() {
-  const { data, error } = useUserInfoQuery(null);
-  const userData = error ? null : data;
+    const { data, error } = useUserInfoQuery(null);
+    const userData = error ? null : data;
 
-  return (
-    <header className="border-b px-4 md:px-6">
-      <div className="max-w-7xl mx-auto">
-        <div className="flex h-16 items-center justify-between gap-4">
-          {/* Left side */}
-          <div className="flex flex-1 items-center gap-2">
-            {/* Mobile menu trigger */}
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  className="group size-8 md:hidden"
-                  variant="ghost"
-                  size="icon"
-                >
-                  <svg
-                    className="pointer-events-none"
-                    width={16}
-                    height={16}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M4 12L20 12"
-                      className="origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
-                    />
-                    <path
-                      d="M4 12H20"
-                      className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
-                    />
-                    <path
-                      d="M4 12H20"
-                      className="origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
-                    />
-                  </svg>
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent align="start" className="w-36 p-1 md:hidden">
-                <NavigationMenu className="max-w-none *:w-full">
-                  <NavigationMenuList className="flex-col items-start gap-0 md:gap-2">
-                    {navigationLinks.map((link, index) => {
-                      return (
-                        <NavigationMenuItem key={index} className="w-full">
-                          <NavigationMenuLink
-                            asChild
-                            className="flex-row items-center gap-2 py-1.5"
-                          >
-                            <Link to={link.href}>{link.label}</Link>
-                          </NavigationMenuLink>
-                        </NavigationMenuItem>
-                      );
-                    })}
-                  </NavigationMenuList>
-                </NavigationMenu>
-              </PopoverContent>
-            </Popover>
-            {/* Logo */}
-            <div className="flex items-center">
-              <Link to="/" className="text-primary hover:text-primary/90">
-                <Logo />
-              </Link>
+    return (
+        <header className="border-b px-4 md:px-6">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex h-16 items-center justify-between gap-4">
+                    {/* Left side */}
+                    <div className="flex flex-1 items-center gap-2">
+                        {/* Mobile menu trigger */}
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <Button
+                                    className="group size-8 md:hidden"
+                                    variant="ghost"
+                                    size="icon"
+                                >
+                                    <svg
+                                        className="pointer-events-none"
+                                        width={16}
+                                        height={16}
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M4 12L20 12"
+                                            className="origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+                                        />
+                                        <path
+                                            d="M4 12H20"
+                                            className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
+                                        />
+                                        <path
+                                            d="M4 12H20"
+                                            className="origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+                                        />
+                                    </svg>
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent
+                                align="start"
+                                className="w-36 p-1 md:hidden"
+                            >
+                                <NavigationMenu className="max-w-none *:w-full">
+                                    <NavigationMenuList className="flex-col items-start gap-0 md:gap-2">
+                                        {navigationLinks.map((link, index) => {
+                                            return (
+                                                <NavigationMenuItem
+                                                    key={index}
+                                                    className="w-full"
+                                                >
+                                                    <NavigationMenuLink
+                                                        asChild
+                                                        className="flex-row items-center gap-2 py-1.5"
+                                                    >
+                                                        <Link to={link.href}>
+                                                            {link.label}
+                                                        </Link>
+                                                    </NavigationMenuLink>
+                                                </NavigationMenuItem>
+                                            );
+                                        })}
+                                    </NavigationMenuList>
+                                </NavigationMenu>
+                            </PopoverContent>
+                        </Popover>
+                        {/* Logo */}
+                        <div className="flex items-center">
+                            <Link
+                                to="/"
+                                className="text-primary hover:text-primary/90"
+                            >
+                                <Logo />
+                            </Link>
+                        </div>
+                    </div>
+                    {/* Middle area */}
+                    <NavigationMenu className="max-md:hidden">
+                        <NavigationMenuList className="gap-2">
+                            {navigationLinks.map((link, index) => {
+                                return (
+                                    <NavigationMenuItem key={index}>
+                                        <NavigationMenuLink
+                                            asChild
+                                            className="text-foreground hover:text-primary flex-row items-center gap-2 py-1.5 font-medium"
+                                        >
+                                            <Link to={link.href}>
+                                                {link.label}
+                                            </Link>
+                                        </NavigationMenuLink>
+                                    </NavigationMenuItem>
+                                );
+                            })}
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                    {/* Right side */}
+                    <div className="flex flex-1 items-center justify-end gap-2">
+                        {userData?.data.email ? (
+                            <NavUserIcon userData={userData} />
+                        ) : (
+                            <Link to="/login">
+                                <Button variant="default">Login</Button>
+                            </Link>
+                        )}
+                    </div>
+                </div>
             </div>
-          </div>
-          {/* Middle area */}
-          <NavigationMenu className="max-md:hidden">
-            <NavigationMenuList className="gap-2">
-              {navigationLinks.map((link, index) => {
-                return (
-                  <NavigationMenuItem key={index}>
-                    <NavigationMenuLink
-                      asChild
-                      className="text-foreground hover:text-primary flex-row items-center gap-2 py-1.5 font-medium"
-                    >
-                      <Link to={link.href}>{link.label}</Link>
-                    </NavigationMenuLink>
-                  </NavigationMenuItem>
-                );
-              })}
-            </NavigationMenuList>
-          </NavigationMenu>
-          {/* Right side */}
-          <div className="flex flex-1 items-center justify-end gap-2">
-            <ModeToggle />
-
-            {userData?.data.email ? (
-              <NavUserIcon userData={userData} />
-            ) : (
-              <Link to="/login">
-                <Button variant="default">Login</Button>
-              </Link>
-            )}
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+        </header>
+    );
 }
